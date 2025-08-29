7635924727f2323f2e20f3fdd6c9b8ea2a406378
Yuri Sanidad <yurisanidad02@gmail.com>
2025-08-29 22:18:41 +0800

    All FORMS

--- WindowsFormsApp1/QueuingForm.cs
+++ WindowsFormsApp1/QueuingForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class QueuingForm : Form
     {
-        public Form1()
+        public QueuingForm()
         {
             InitializeComponent();
         }
